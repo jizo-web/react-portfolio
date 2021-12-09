954a9a55b080a23edadb8f3baec05f321c387e61
t_mura <jizo-web@outlook.jp>
2021-12-10 05:47:07 +0900

    fix portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+// 画像をそれぞれimportします。パスに注意してください。
+import ProfileImage from '../images/IMG_0383.JPG';
+import CoverImage from '../images/AdobeStock_402438351.jpeg';
+import { FaTwitter, FaGithub } from 'react-icons/fa';
+export const Header = () => {
+    return (
+        <header className="main-cover" style={{ backgroundImage: `url(${CoverImage})` }}>
+            {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+            <div className="overlay"></div>
+            <div className="container">
+                <div className="display-table">
+                    <div className="display-table-contents">
+                        {/* カバー画像 */}
+                        <div className="profile-thumb" style={{ backgroundImage: `url(${ProfileImage})` }}></div>
+                        {/* 名前と肩書はみなさんのお名前や肩書を自由に入れてください */}
+                        <h1 className="title-text">あなたの名前</h1>
+                        <h3 className="title-text">あなたの肩書(ex.Engineer)</h3>
+                        <ul className="social-icons">
+                            <li className="icon-link">
+                                {/* リンク先(href)は任意のURLを設定してください */}
+                                <a href="https://twitter.com/">
+                                    <FaTwitter color="white" size="2rem" />
+                                </a>
+                            </li>
+                            <li className="icon-link">
+                                {/* リンク先(href)は任意のURLを設定してください */}
+                                <a href="https://github.com/">
+                                    <FaGithub color="white" size="2rem" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
